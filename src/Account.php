e2063445a2fd5379acef493e6d52064fb51faa31
tenshiPure <user.ryo@gmail.com>
2015-07-08 22:15:16 +0900

    フォーマッタ適用

--- src/Account.php
+++ src/Account.php
@@ -2,79 +2,79 @@
 
 class Account
 {
-	/** @var string */
-	private $code;
+    /** @var string */
+    private $code;
 
-	/** @var string */
-	private $announce;
+    /** @var string */
+    private $announce;
 
-	/** @var int */
-	private $discount;
+    /** @var int */
+    private $discount;
 
-	/**
-	 * @param string $code
-	 * @param string $announce
-	 * @param string $discount
+    /**
+     * @param string $code
+     * @param string $announce
+     * @param string $discount
      */
-	private function __construct($code, $announce, $discount)
-	{
-		$this->code = $code;
-		$this->announce = $announce;
-		$this->discount = $discount;
-	}
+    private function __construct($code, $announce, $discount)
+    {
+        $this->code = $code;
+        $this->announce = $announce;
+        $this->discount = $discount;
+    }
 
-	/**
-	 * @param string $input
-	 * @return Account
-	 *
-	 * @throws Exception
+    /**
+     * @param string $input
+     * @return Account
+     *
+     * @throws Exception
      */
-	public static function create($input)
-	{
-		switch ($input) {
-			case 'normal' :
-				return new Account('normal', '【プレミアム会員になりませんか】', 0);
+    public static function create($input)
+    {
+        switch ($input) {
+            case 'normal' :
+                return new Account('normal', '【プレミアム会員になりませんか】', 0);
 
-			case 'premium' :
-				return new Account('premium', '【プレミアム会員の継続利用について】', 1000);
+            case 'premium' :
+                return new Account('premium', '【プレミアム会員の継続利用について】', 1000);
 
-			case 'rich' :
-				return new Account('rich', '【リッチプレミアム会員の継続利用について】', 2000);
+            case 'rich' :
+                return new Account('rich', '【リッチプレミアム会員の継続利用について】', 2000);
 
-			default :
-				throw new Exception("不正な会員種別指定です\n");
+            default :
+                throw new Exception("不正な会員種別指定です\n");
         }
-	}
+    }
 
-	/**
-	 * @return int
+    /**
+     * @return int
      */
-	public function getDiscount()
-	{
-		return $this->discount;
-	}
+    public function getDiscount()
+    {
+        return $this->discount;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function isPremium()
-	{
-		return $this->code === 'premium';
-	}
-
-	/**
-	 * @return bool
+    /**
+     * @return bool
      */
-	public function isRich()
-	{
-		return $this->code === 'rich';
-	}
+    public function isPremium()
+    {
+        return $this->code === 'premium';
+    }
 
-	/**
-	 * @return string
+    /**
+     * @return bool
      */
-	public function __toString()
-	{
-		return "お知らせ：{$this->announce}\n\n";
-	}
+    public function isRich()
+    {
+        return $this->code === 'rich';
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return "お知らせ：{$this->announce}\n\n";
+    }
 }
--- src/controller.php
+++ src/controller.php
@@ -6,15 +6,15 @@ require_once 'model.php';
 $model = new Model();
 
 try {
-	$account = $model->getAccount();
-	$currentPlan = $model->getCurrentPlan($account);
-	$nextPlan = $model->getNextPlan($account);
+    $account = $model->getAccount();
+    $currentPlan = $model->getCurrentPlan($account);
+    $nextPlan = $model->getNextPlan($account);
 
-	$model->change($currentPlan, $nextPlan);
+    $model->change($currentPlan, $nextPlan);
 
-	echo $nextPlan;
-	echo $account;
+    echo $nextPlan;
+    echo $account;
 
 } catch (Exception $e) {
-	echo $e->getMessage();
+    echo $e->getMessage();
 }
